41cf1fd06b949e0b7ea8be9a43009ca7d76e483b
Adam Brown <adam.brown@tapiit.com>
2020-08-04 14:07:47 +0100

    Added Drag and Drop Sample with source

---
+++ BlazorSampleComponents/Models/TodoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSampleComponents.Models
+{
+	public class TodoModel
+	{
+		public int Id { get; set; }
+		public TodoStatuses Status { get; set; }
+		public string Description { get; set; }
+		public DateTime LastUpdated { get; set; }
+	}
+
+	public enum TodoStatuses
+	{
+		Todo,
+		InProgress,
+		Completed
+	}
+}
